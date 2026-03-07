6425d95f29ce0c503854cb7da28249a5e47ae205
Vignesh <vignesh.code93@gmail.com>
2026-03-07 21:32:21 +0530

    basic template form

--- angular_routes/src/app/app.config.ts
+++ angular_routes/src/app/app.config.ts
@@ -7,5 +7,5 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimationsAsync('noop')]
+    provideAnimationsAsync('noop'), provideAnimationsAsync('noop')]
 };
--- angular_routes/src/app/components/dashboard/dashboard.component.ts
+++ angular_routes/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 
+interface Gender {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule ,MatSelectModule ,MatButtonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+ Gender: Gender[]= [
+  {value: 'male', viewValue: 'Male'},
+  {value: 'female', viewValue: 'Female'} ];
 
+
+onSubmit(form: NgForm) {
+    console.log("Form Data", form.value);
+  }
 }
